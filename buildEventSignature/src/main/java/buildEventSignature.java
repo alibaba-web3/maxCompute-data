b5009fedb95adf92095c6aab5fc4d219079046cb
pawpawya <18143486449@163.com>
2023-03-09 10:21:25 +0800

    init

---
+++ buildEventSignature/src/main/java/buildEventSignature.java
@@ -0,0 +1,9 @@
+import com.aliyun.odps.udf.UDF;
+import org.web3j.abi.EventEncoder;
+
+public class buildEventSignature extends UDF {
+    // TODO define parameters and return type, e.g:  public String evaluate(String a, String b)
+    public String evaluate(String s) {
+        return EventEncoder.buildEventSignature(s);
+    }
+}
