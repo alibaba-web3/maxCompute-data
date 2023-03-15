d710de7918f43acc00148ab6f02616d07be62109
pawpawya <18143486449@163.com>
2023-03-15 19:21:12 +0800

    common

--- common/src/main/java/com/aliyun/odps/web3/common/TypeConvertUtils.java
+++ common/src/main/java/com/aliyun/odps/web3/common/TypeConvertUtils.java
@@ -1,9 +1,10 @@
 package com.aliyun.odps.web3.common;
 
 import org.web3j.abi.TypeEncoder;
-import org.web3j.abi.datatypes.BytesType;
-import org.web3j.abi.datatypes.Type;
+import org.web3j.abi.datatypes.*;
+import org.web3j.utils.Numeric;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,12 +15,39 @@ import java.util.stream.Collectors;
 public class TypeConvertUtils {
 
     public static String convert2String(Type parameter) {
-        if (parameter instanceof BytesType) {
-            return TypeEncoder.encode(parameter);
-        } else {
+        if (parameter instanceof NumericType) {
             return String.valueOf(parameter.getValue());
+        } else if (parameter instanceof Address) {
+            return String.valueOf(parameter.getValue());
+        } else if (parameter instanceof DynamicBytes) {
+            return Numeric.toHexString(((DynamicBytes) parameter).getValue());
+        } else if (parameter instanceof StaticArray) {
+            List<Type> list = ((StaticArray) parameter).getValue();
+            return convert2String(list).toString();
+        } else if (parameter instanceof DynamicArray) {
+            List<Type> list = ((DynamicArray) parameter).getValue();
+            return convert2String(list).toString();
+        } else {
+            return TypeEncoder.encode(parameter);
         }
 
+    }
+
+    public static List<String> convert2StringList(Type parameter) {
+        List<Type> list;
+        if (parameter instanceof StaticArray) {
+            list = ((StaticArray) parameter).getValue();
+        } else if (parameter instanceof DynamicArray) {
+            list = ((DynamicArray) parameter).getValue();
+        } else {
+            throw new UnsupportedOperationException(
+                    "Type cannot be encoded by 'convert2StringList': " + parameter.getClass());
+        }
+        List<String> result = new ArrayList<>(list.size());
+        for (Type type : list) {
+            result.add(convert2String(type));
+        }
+        return result;
     }
 
     public static List<String> convert2String(List<Type> list) {
